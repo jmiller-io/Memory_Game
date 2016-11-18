3b1a16d080aa6311cde59b2b919ade9b2fee15a8
Jake Miller <jakemiller@Jakes-MacBook-Pro.local>
2016-11-17 18:16:31 -0800

    Add Card Logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,3 +6,27 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+
+//Compare Queen with King
+if (cardTwo === cardFour){
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+};
+
+//Compare Kings
+if (cardFour === cardThree){
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+};
+
+// Compare Queens
+if (cardTwo === cardOne){
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+};
