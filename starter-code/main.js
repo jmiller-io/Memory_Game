9d6c30d57723f0376ac09504c34f8a89b1b606c9
Jake Miller <jakemiller@Jakes-MacBook-Pro.local>
2016-11-17 19:21:15 -0800

    Updated Javascript Logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -11,20 +11,12 @@ var cardFour = "king";
 if (cardTwo === cardFour){
 	alert("You found a match!");
 }
-else {
-	alert("Sorry, try again.");
-};
-
 //Compare Kings
-if (cardFour === cardThree){
-	alert("You found a match!");
+else if (cardThree === cardFour) {
+	alert("You found a match!")
 }
-else {
-	alert("Sorry, try again.");
-};
-
-// Compare Queens
-if (cardTwo === cardOne){
+//Compare Queens
+else if (cardTwo === cardOne){
 	alert("You found a match!");
 }
 else {
