552eee8a24c24c550701c0a91efdab89287c90db
Jake Miller <jakemiller@Jakes-MacBook-Pro.local>
2016-11-21 14:44:14 -0800

    Commenting out code

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,13 +1,11 @@
-console.log("JS file is connected to HTML! Woo!")
-
-//Cards Array
+// Create and Populate Cards Array
 var cards = ["queen","queen","king","king"];
 
-// Cards in Play Array
+// Create CardsinPlay Array
 var cardsInPlay = [];
 
 
-// Card Board Creation Logic
+// Game Board Creation Function
 var createBoard = function () {
 	for (var i = 0; i<cards.length; i++) {
 		var cardElement = document.createElement('div');
@@ -15,16 +13,17 @@ var createBoard = function () {
 
 		var gBoard = document.getElementById('game-board');
 		gBoard.appendChild(cardElement);
-		
+
+		// Set cardElement attribute to card value 
 		cardElement.setAttribute('data-card', cards[i]);
 
-		// when a card is clicked the function isTwoCards will be executed
+		// Add Card Click Event Listener
     	cardElement.addEventListener('click', isTwoCards)
 	}
 };
 
 
-// isMatch Function
+// Logic for Determining Card Value Match
 var isMatch = function (){
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!");
@@ -34,29 +33,32 @@ var isMatch = function (){
 };
 
 
-// isTwoCards Function
+// Determines When Two Cards are Clicked On and "flips" the Card by Assiging an img
 var isTwoCards = function (){
+  // Adds card to cardsInPlay array
   cardsInPlay.push(this.getAttribute('data-card'));
-  console.log(this.getAttribute('data-card'));
+
+  // Assigns img based on data-card attribute
   if (this.getAttribute('data-card') === 'king'){
   	  this.innerHTML = '<img src="king.jpg" alt="King of the Jungle"/>';
   } else {
   	  this.innerHTML = '<img src="queen.jpg" alt="Queen of the Jungle" />';
   };
 
-
+  // Determines when two cards have been clicked, compares them and clears the cardsInPlay array
   if (cardsInPlay.length === 2) {
-
-    isMatch(cardsInPlay);
-
-    cardsInPlay = [];
-
+	 isMatch(cardsInPlay);
+	 cardsInPlay = [];
   }
 };
 
 
-// Create Board Elements Function
+// Creates the Game Board
 createBoard(cards);
+
+
+
+
 
 
 /* Unused Code built from previous lessons
