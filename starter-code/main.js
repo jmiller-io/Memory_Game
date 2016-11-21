d7016ef842f33d8d238b20d28dda46c97bc3da63
Jake Miller <jakemiller@Jakes-MacBook-Pro.local>
2016-11-21 14:32:29 -0800

    Modified Javascript game logic. Functional Game.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,14 +1,67 @@
 console.log("JS file is connected to HTML! Woo!")
 
-//Create Card Variables
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//Cards Array
+var cards = ["queen","queen","king","king"];
+
+// Cards in Play Array
+var cardsInPlay = [];
 
 
+// Card Board Creation Logic
+var createBoard = function () {
+	for (var i = 0; i<cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+
+		var gBoard = document.getElementById('game-board');
+		gBoard.appendChild(cardElement);
+		
+		cardElement.setAttribute('data-card', cards[i]);
+
+		// when a card is clicked the function isTwoCards will be executed
+    	cardElement.addEventListener('click', isTwoCards)
+	}
+};
+
+
+// isMatch Function
+var isMatch = function (){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+};
+
+
+// isTwoCards Function
+var isTwoCards = function (){
+  cardsInPlay.push(this.getAttribute('data-card'));
+  console.log(this.getAttribute('data-card'));
+  if (this.getAttribute('data-card') === 'king'){
+  	  this.innerHTML = '<img src="king.jpg" alt="King of the Jungle"/>';
+  } else {
+  	  this.innerHTML = '<img src="queen.jpg" alt="Queen of the Jungle" />';
+  };
+
+
+  if (cardsInPlay.length === 2) {
+
+    isMatch(cardsInPlay);
+
+    cardsInPlay = [];
+
+  }
+};
+
+
+// Create Board Elements Function
+createBoard(cards);
+
+
+/* Unused Code built from previous lessons
 //Compare Queen with King
-/* 
+
 if (cardTwo === cardFour){
 	alert("You found a match!");
 }
@@ -23,17 +76,12 @@ else if (cardTwo === cardOne){
 else {
 	alert("Sorry, try again.");
 };
+
+
+//Create Card Variables
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
+
 */
-
-
-var createCards = function (numCards) {
-	for (i=1; i<=numCards; i++) {
-		var newDiv = document.createElement('div');
-		newDiv.className = 'card';
-
-		var gBoard = document.getElementById('game-board');
-		gBoard.appendChild(newDiv);
-	}
-}
-
-createCards(4);
