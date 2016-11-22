f1e52efb8ec7348824d5cea029dd67198301b555
Jake Miller <jakemiller@Jakes-MacBook-Pro.local>
2016-11-22 10:28:15 -0800

    Add card shuffling ability

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,9 +2,32 @@
 
 // Create and Populate Cards Array
 var cards = ["queen","queen","king","king"];
+var cards = shuffle(cards);
 
 // Create CardsinPlay Array
 var cardsInPlay = [];
+
+
+
+// Function for Shuffling cards
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 
 // Game Board Creation Function
